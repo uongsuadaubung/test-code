690f52db4b3565fe9c722991c8dde5bfc7011fc4
uongsuadaubung <manhkien13041997@gmail.com>
2024-06-07 23:50:11 +0700

    chore: update package

--- DataStruct.ts
+++ DataStruct.ts
@@ -1,8 +1,8 @@
 export class Heap<T> {
-  private readonly heap: { item: T, priority: number }[];
-  private readonly heapType: 'min' | 'max';
+  private readonly heap: { item: T; priority: number }[];
+  private readonly heapType: "min" | "max";
 
-  constructor(type: 'min' | 'max') {
+  constructor(type: "min" | "max") {
     this.heap = [];
     this.heapType = type;
   }
@@ -31,15 +31,15 @@ export class Heap<T> {
     return this.getParentIndex(index) >= 0;
   }
 
-  private leftChild(index: number): { item: T, priority: number } {
+  private leftChild(index: number): { item: T; priority: number } {
     return this.heap[this.getLeftChildIndex(index)];
   }
 
-  private rightChild(index: number): { item: T, priority: number } {
+  private rightChild(index: number): { item: T; priority: number } {
     return this.heap[this.getRightChildIndex(index)];
   }
 
-  private parent(index: number): { item: T, priority: number } {
+  private parent(index: number): { item: T; priority: number } {
     return this.heap[this.getParentIndex(index)];
   }
 
@@ -53,9 +53,10 @@ export class Heap<T> {
     let index = this.length - 1;
     while (this.hasParent(index)) {
       const parentIndex = this.getParentIndex(index);
-      const shouldSwap = this.heapType === 'max'
-        ? this.parent(index).priority < this.heap[index].priority
-        : this.parent(index).priority > this.heap[index].priority;
+      const shouldSwap =
+        this.heapType === "max"
+          ? this.parent(index).priority < this.heap[index].priority
+          : this.parent(index).priority > this.heap[index].priority;
 
       if (shouldSwap) {
         this.swap(parentIndex, index);
@@ -71,18 +72,23 @@ export class Heap<T> {
     while (this.hasLeftChild(index)) {
       let chosenChildIndex = this.getLeftChildIndex(index);
       if (this.hasRightChild(index)) {
-        if (this.heapType === 'max') {
-          if (this.rightChild(index).priority > this.leftChild(index).priority) {
+        if (this.heapType === "max") {
+          if (
+            this.rightChild(index).priority > this.leftChild(index).priority
+          ) {
             chosenChildIndex = this.getRightChildIndex(index);
           }
-        } else if (this.rightChild(index).priority < this.leftChild(index).priority) {
+        } else if (
+          this.rightChild(index).priority < this.leftChild(index).priority
+        ) {
           chosenChildIndex = this.getRightChildIndex(index);
         }
       }
 
-      const shouldSwap = this.heapType === 'max'
-        ? this.heap[index].priority < this.heap[chosenChildIndex].priority
-        : this.heap[index].priority > this.heap[chosenChildIndex].priority;
+      const shouldSwap =
+        this.heapType === "max"
+          ? this.heap[index].priority < this.heap[chosenChildIndex].priority
+          : this.heap[index].priority > this.heap[chosenChildIndex].priority;
 
       if (shouldSwap) {
         this.swap(index, chosenChildIndex);
@@ -94,7 +100,7 @@ export class Heap<T> {
   }
 
   public insert(item: T, priority: number): void {
-    this.heap.push({item, priority});
+    this.heap.push({ item, priority });
     this.heapifyUp();
   }
 
@@ -118,5 +124,67 @@ export class Heap<T> {
 
   public get length(): number {
     return this.heap.length;
+  }
+}
+
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+class TrieNode {
+  public children: Map<string, TrieNode>;
+  public isEndOfWord: boolean;
+
+  constructor() {
+    this.children = new Map<string, TrieNode>();
+    this.isEndOfWord = false;
+  }
+}
+
+export class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string): void {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+      node = node.children.get(char)!;
+    }
+    node.isEndOfWord = true;
+  }
+
+  search(word: string): boolean {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        return false;
+      }
+      node = node.children.get(char)!;
+    }
+    return node.isEndOfWord;
+  }
+
+  startsWith(prefix: string): boolean {
+    let node = this.root;
+    for (const char of prefix) {
+      if (!node.children.has(char)) {
+        return false;
+      }
+      node = node.children.get(char)!;
+    }
+    return true;
   }
 }
