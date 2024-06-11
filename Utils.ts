6e1ef86f226bd06220e998f9de91ba189ec8fcf9
kien.hm <kien.hm@clickcure.vn>
2024-06-11 08:26:04 +0700

    feat: move local to git

--- Utils.ts
+++ Utils.ts
@@ -1,4 +1,4 @@
-import {TreeNode} from "./DataStruct";
+import { ListNode, TreeNode } from "./DataStruct";
 
 function insertIntoBST(root: TreeNode | null, val: number): TreeNode {
   if (root === null) {
@@ -23,4 +23,52 @@ export function arrayToBST(arr: Array<number|null>): TreeNode | null {
     }
   }
   return root;
+}
+
+export const createListNodeFromArray = (arr: number[]): ListNode | null => {
+  if (arr.length === 0) {
+    return null
+  }
+  const head = new ListNode(arr[0])
+  let current = head
+  for (let i = 1; i < arr.length; i++) {
+    current.next = new ListNode(arr[i])
+    current = current.next
+  }
+  return head
+}
+function generateSubsetsBacktrack(nums: number[]): number[][] {
+  const n = nums.length;
+  const subsets: number[][] = [];
+  
+  const backtrack = (start: number, path: number[]) =>{
+    subsets.push([...path]);
+    for (let i = start; i < nums.length; i++) {
+      path.push(nums[i]);
+      backtrack(i+1, path);
+      path.pop();
+    }
+    
+  }
+  
+  backtrack(0,[])
+  
+  return subsets;
+}
+
+function generateSubsetsBinary(nums: number[]): number[][] {
+  const n = nums.length;
+  const subsets: number[][] = [];
+  
+  for (let i = 0; i < (1 << n); i++) {
+    const subset: number[] = [];
+    for (let j = 0; j < n; j++) {
+      if ((i & (1 << j)) !== 0) {
+        subset.push(nums[j]);
+      }
+    }
+    subsets.push(subset);
+  }
+  
+  return subsets;
 }
