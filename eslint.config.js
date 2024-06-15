a6b4537dd737e19f7a03e52be0e7143685bf26cf
uongsuadaubung <manhkien13041997@gmail.com>
2024-06-16 00:39:23 +0700

    chore: update package

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      eqeqeq: "error", // sử dụng === thay vì ==
+      "no-eval": "error", // không sử dụng eval
+      "consistent-return": "error", // nhất quán khi sử dụng return
+      "no-duplicate-imports": "error", // sử dụng import 1 lần
+      //variables
+      "no-shadow": "error", // Ngăn chặn việc khai báo biến có cùng tên với các biến trong phạm vi ngoài.
+      "no-redeclare": "error", // không khai báo lại biến
+      //styles
+      indent: ["error", 2], // 2 khoảng trắng
+      semi: ["error", "always"], // bắt buộc đặt dấu ;
+      quotes: ["error", "double"], // sử dụng "
+      "comma-dangle": ["error", "always-multiline"],
+      "max-len": ["error", { code: 120 }],
+      "arrow-parens": ["error", "as-needed"],
+      //typescript
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn",
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
+      ], // bắt buộc dùng kiểu trả về
+    },
+  },
+];
