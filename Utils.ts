14ae829b95e1ad2633676f60ff697b4ab1f75c19
kien.hm <kien.hm@clickcure.vn>
2024-06-25 11:26:43 +0700

    feat: update package

--- Utils.ts
+++ Utils.ts
@@ -108,3 +108,30 @@ export function rotateMatrix(matrix: number[][]): void {
     }
   }
 }
+
+export function arrayToTreeNode(arr: (number | null)[]): TreeNode | null {
+  if (!arr.length) return null;
+  
+  const root = new TreeNode(arr[0]!);
+  const queue: (TreeNode | null)[] = [root];
+  let i = 1;
+  
+  while (queue.length && i < arr.length) {
+    const current = queue.shift()!;
+    
+    if (current !== null) {
+      if (arr[i] !== null) {
+        current.left = new TreeNode(arr[i]!);
+        queue.push(current.left);
+      }
+      i++;
+      if (i < arr.length && arr[i] !== null) {
+        current.right = new TreeNode(arr[i]!);
+        queue.push(current.right);
+      }
+      i++;
+    }
+  }
+  
+  return root;
+}
